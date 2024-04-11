187ec8179a262bba418aeeba97d5112f254a9fd1
joytan12 <juan.garciamo@sansano.usm.cl>
2024-04-11 17:29:59 -0400

    comentario simple

---
+++ Redes/lab 3/servidor.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+func handleDNSRequest(conn *net.UDPConn) {
+	buffer := make([]byte, 1024)
+
+	n, addr, err := conn.ReadFromUDP(buffer)
+	if err != nil {
+		fmt.Println("Error reading from UDP connection:", err)
+		return
+	}
+
+	fmt.Println("Received DNS request from", addr)
+
+	domain := strings.TrimSpace(string(buffer[:n]))
+
+	ip, err := net.LookupIP(domain)
+	if err != nil {
+		fmt.Println("Error looking up IP for", domain, ":", err)
+		return
+	}
+
+	fmt.Println("Resolved", domain, "to", ip[0])
+
+	response := []byte(ip[0].String())
+	_, err = conn.WriteToUDP(response, addr)
+	if err != nil {
+		fmt.Println("Error writing response to UDP connection:", err)
+		return
+	}
+}
+
+func main() {
+	udpAddr, err := net.ResolveUDPAddr("udp", ":53")
+	if err != nil {
+		fmt.Println("Error resolving UDP address:", err)
+		os.Exit(1)
+	}
+
+	conn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		fmt.Println("Error listening on UDP port 53:", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	fmt.Println("DNS server listening on port 53...")
+
+	for {
+		handleDNSRequest(conn)
+	}
+}
